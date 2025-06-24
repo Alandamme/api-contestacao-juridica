094ab957398a6167a0f32c5359841fb4feac5533
Alandamme <amillar@ldadv.com.br>
2025-06-24 13:49:30 -0300

    Delete debug_print.py

--- debug_print.py
+++
@@ -1,5 +0,0 @@
-import os
-
-caminho = "/opt/render/project/src/static/modelos"
-print("Listando arquivos em:", caminho)
-print(os.listdir(caminho) if os.path.exists(caminho) else "Caminho não existe")
